8e5eaca22638561e241c4de26732860d3c2d8d41
bluelovers <codelovers@users.sourceforge.net>
2024-05-20 19:29:36 +0800

    ci: minify in ci

---
+++ build.ts
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+import { build } from 'esbuild'
+import { join } from 'path'
+
+import { sassPlugin } from 'esbuild-sass-plugin'
+import { __ROOT, __ROOT_OUTPUT } from './test/__root';
+
+build({
+	entryPoints: [
+		join(__ROOT, 'src/index.mts')
+	],
+	outfile: join(__ROOT_OUTPUT, 'javascript', 'sd-webui-pnginfo-beautify.js'),
+	bundle: true,
+	plugins: [
+		sassPlugin({
+			type: "css-text",
+		}),
+	],
+	platform: 'browser',
+	treeShaking: true,
+	sourcemap: true,
+	// @ts-ignore
+	//analyze: true,
+	legalComments: 'none',
+	allowOverwrite: true,
+	minifySyntax: true,
+	format: 'iife',
+	minify: Boolean(process.env['ESBUILD_MINIFY'])
+})
